3aa0a37a2507fdb6c99e33d10f264d1159627dd9
rlacksgus97 <rlacksgus97@naver.com>
2022-04-25 17:10:47 +0900

    [feat] : 수집한 임상 정보 및 목록 조회 API

--- src/main/java/com/bd/assignment3/research/dto/ReadResearchResDto.java
+++ src/main/java/com/bd/assignment3/research/dto/ReadResearchResDto.java
@@ -1,2 +1,26 @@
-package com.bd.assignment3.research.dto;public class ReadResearchResDto {
+package com.bd.assignment3.research.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ReadResearchResDto {
+    private String num;
+
+    private String title;
+
+    private String department;
+
+    private String organization;
+
+    private String participantsNum;
+
+    private String term;
+
+    private String type;
+
+    private String stage;
+
+    private String range;
 }
--- src/main/java/com/bd/assignment3/research/dto/SimpleResearchResDto.java
+++ src/main/java/com/bd/assignment3/research/dto/SimpleResearchResDto.java
@@ -1,2 +1,29 @@
-package com.bd.assignment3.research.dto;public class SimpleResearchResDto {
+package com.bd.assignment3.research.dto;
+
+import com.bd.assignment3.research.Research;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class SimpleResearchResDto {
+    private String num;
+
+    private String title;
+
+    private String department;
+
+    private String organization;
+
+    private String term;
+
+    public static SimpleResearchResDto toDto(Research research) {
+        return SimpleResearchResDto.builder()
+                .num(research.getNum())
+                .title(research.getTitle())
+                .department(research.getDepartment())
+                .organization(research.getOrganization())
+                .term(research.getTerm())
+                .build();
+    }
 }
